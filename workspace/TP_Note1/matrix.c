286e95d7a6f36a9d825239d9d0d8a32bff561f87
poirotman <92015869+poirotman@users.noreply.github.com>
2021-10-18 08:51:24 +0000

    Je n'ai fait que la question 1

---
+++ workspace/TP_Note1/matrix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct Matrix{
+    int nblignecolonne[2];
+    int **Matrice;
+};
+typedef struct Matrix Matrix;
+Matrix *MatrixArray;
+
+Matrix readMatrix(){
+    int lignes;
+    int colonnes;
+    scanf("%i %i", &lignes, &colonnes);
+    Matrix matrice;
+    matrice.nblignecolonne[0] = lignes;
+    matrice.nblignecolonne[1] = colonnes;
+    matrice.Matrice=calloc(lignes, sizeof(int*) );
+    for(int i=0; i<lignes; i++){
+        matrice.Matrice[i]=(int*)calloc(colonnes, sizeof(int));
+    };
+    for(int i =0; i<lignes; i++){
+        for(int y=0; y<colonnes; y++){
+            scanf("%i", &(matrice.Matrice[i][y]));
+        }
+    }
+    return matrice;
+}
+
+int main(void) {
+    int nbMatrice;
+    scanf("%i", &nbMatrice);
+    Matrix mat;
+    mat = readMatrix();
+    printf("%i", mat.Matrice[0][0]);
+    return 0;
+}
